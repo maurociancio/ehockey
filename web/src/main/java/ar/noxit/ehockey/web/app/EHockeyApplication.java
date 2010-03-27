a6504b032f4d519e212ed23eb038a5bd44f69e91
Mauro Ciancio <maurociancio@gmail.com>
2010-03-27 15:53:20 -0300

    instanciar el listener de creacion de componentes para wicket (re #6)

--- web/src/main/java/ar/noxit/ehockey/web/app/EHockeyApplication.java
+++ web/src/main/java/ar/noxit/ehockey/web/app/EHockeyApplication.java
@@ -5,6 +5,7 @@ import ar.noxit.ehockey.web.pages.HomePage;
 import org.apache.commons.lang.Validate;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
 /**
  * Application object for your web application. If you want to run this
@@ -22,6 +23,13 @@ public class EHockeyApplication extends WebApplication {
     public EHockeyApplication(String appMode) {
         Validate.notNull(appMode, "application mode cannot be null");
         this.appMode = appMode;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+
+        addComponentInstantiationListener(new SpringComponentInjector(this));
     }
 
     /**
