ade7bcfa1890f4b87077a7ff4ca26db6af05f173
Mauro Ciancio <maurociancio@gmail.com>
2010-03-27 15:13:19 -0300

    mover aplicacion de package (re #6)

---
+++ web/src/main/java/ar/noxit/ehockey/web/app/EHockeyApplication.java
@@ -0,0 +1,29 @@
+package ar.noxit.ehockey.web.app;
+
+import ar.noxit.ehockey.HomePage;
+import ar.noxit.ehockey.StartJetty;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this
+ * application without deploying, run the Start class.
+ * 
+ * @see StartJetty.myproject.Start#main(String[])
+ */
+public class EHockeyApplication extends WebApplication {
+
+    /**
+     * Constructor
+     */
+    public EHockeyApplication() {
+    }
+
+    /**
+     * @see wicket.Application#getHomePage()
+     */
+    @Override
+    public Class<? extends WebPage> getHomePage() {
+        return HomePage.class;
+    }
+}
