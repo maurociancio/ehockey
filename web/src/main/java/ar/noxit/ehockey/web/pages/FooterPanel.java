a12f267b329e777c7bae067ec774d47962f45ef0
Mauro Ciancio <maurociancio@gmail.com>
2010-03-28 20:34:08 -0300

    footer panel (see #4)

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/FooterPanel.java
@@ -0,0 +1,10 @@
+package ar.noxit.ehockey.web.pages;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class FooterPanel extends Panel {
+
+    public FooterPanel(String id) {
+        super(id);
+    }
+}
