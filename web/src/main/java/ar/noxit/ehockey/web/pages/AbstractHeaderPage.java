79581e669180646032fd4a41bb6a1ad99b51895f
Mauro Ciancio <maurociancio@gmail.com>
2010-03-28 20:33:39 -0300

    pagina con header (see #4)

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/AbstractHeaderPage.java
@@ -0,0 +1,8 @@
+package ar.noxit.ehockey.web.pages;
+
+public abstract class AbstractHeaderPage extends AbstractBasePage {
+
+    public AbstractHeaderPage() {
+        add(new HeaderPanel("header"));
+    }
+}
