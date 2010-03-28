bccb9dca2deea8c7fa17f2df60803e6796bdedb7
Mauro Ciancio <maurociancio@gmail.com>
2010-03-28 20:27:00 -0300

    header panel (see #4)

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/HeaderPanel.java
@@ -0,0 +1,10 @@
+package ar.noxit.ehockey.web.pages;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class HeaderPanel extends Panel {
+
+    public HeaderPanel(String id) {
+        super(id);
+    }
+}
