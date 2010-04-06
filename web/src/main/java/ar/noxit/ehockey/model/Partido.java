6c132e58ede11556cfb5a45a95f0d6a703434f7b
Tito <tito@tito-laptop.(none)>
2010-04-06 17:04:28 -0300

    Entidades básicas de Partido y Planilla see #62

---
+++ web/src/main/java/ar/noxit/ehockey/model/Partido.java
@@ -0,0 +1,28 @@
+package ar.noxit.ehockey.model;
+
+public class Partido {
+    Equipo local;
+    Equipo visitante;
+
+    Planilla planillaPrecargada;
+    Planilla planillaFinal;
+
+    public Partido(Equipo local, Equipo visitante) {
+        this.local = local;
+        this.visitante = visitante;
+    }
+
+    /**
+     * La primera vez se crea la planilla, luego se devuelve siempre la misma.
+     * Esto asegura que la planilla del partido sea la misma en cualquier
+     * momento.
+     * 
+     * @return planilla del partido precargada
+     */
+    public Planilla getPlanillaPrecargada() {
+        if (planillaPrecargada == null) {
+            planillaPrecargada = new Planilla(local, visitante);
+        }
+        return planillaPrecargada;
+    }
+}
---
+++ web/src/main/java/ar/noxit/ehockey/model/Planilla.java
@@ -0,0 +1,13 @@
+package ar.noxit.ehockey.model;
+
+public class Planilla {
+    Equipo local;
+    Equipo visitante;
+    
+    public Planilla(Equipo local, Equipo visitante) {
+        this.local = local;
+        this.visitante = visitante;
+    }
+    
+    
+}
