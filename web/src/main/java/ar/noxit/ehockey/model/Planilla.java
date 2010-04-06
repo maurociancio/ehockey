0a781762ccd162fa553d55144ba2b4131c3c335c
Tito <tito@tito-laptop.(none)>
2010-04-06 17:36:28 -0300

    Agregando atributos a la clase planilla. Faltan agregar otros atributos y definir bien los tipos. Estoy pensando en que la planilla que se define para un partido como precargada y la final son el mismo tipo de entidad. Solo que se va a tener que clonar la planilla precargada para modificarla y guardar la final. Esto para garantizar que la planilla inicial que se obtenga en cualquier momento sea siempre la misma.
    see #37
    see #62

--- web/src/main/java/ar/noxit/ehockey/model/Planilla.java
+++ web/src/main/java/ar/noxit/ehockey/model/Planilla.java
@@ -1,13 +1,37 @@
 package ar.noxit.ehockey.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Planilla {
     Equipo local;
     Equipo visitante;
+
+    String dTL;
+    String capitanL;
+    String pFisicoL;
+    String medicoL;
+    String juezDeMesaL;
+    String arbitroL;
+
+    String dTV;
+    String capitanV;
+    String pFisicoV;
+    String medicoV;
+    String juezDeMesaV;
+    String arbitroV;
+
+    String observaciones;
     
+    //TODO definir el atributo fecha
+    //TODO definir el resto de los atributos que son: Torneo, Rueda, Partido, Sector(damas/caballeros), categoría, división, zona
+
+    List<Jugador> goleadoresLocal = new ArrayList<Jugador>();
+    List<Jugador> goleadoresVisitante = new ArrayList<Jugador>();
+
     public Planilla(Equipo local, Equipo visitante) {
         this.local = local;
         this.visitante = visitante;
     }
-    
-    
+
 }
