3857f03e3fb24627d96a2e7ab088284fefe3a995
Mauro Ciancio <maurociancio@gmail.com>
2010-04-06 15:18:47 -0300

    excepcion cuando el jugador se agrega 2 veces a la misma lista de buena fe
    see #97

---
+++ web/src/main/java/ar/noxit/ehockey/exception/JugadorYaPerteneceAListaException.java
@@ -0,0 +1,38 @@
+package ar.noxit.ehockey.exception;
+
+/**
+ * Excepción lanzada cuando un jugador se agrega dos veces a la misma lista de
+ * buena fe.
+ * 
+ * @author Mauro Ciancio
+ * 
+ */
+public class JugadorYaPerteneceAListaException extends ReglaNegocioException {
+
+    /**
+     * {@inheritDoc}
+     */
+    public JugadorYaPerteneceAListaException() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public JugadorYaPerteneceAListaException(String message) {
+        super(message);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public JugadorYaPerteneceAListaException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public JugadorYaPerteneceAListaException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
