5efa556ed9c5f1df137deb27d89885c6081e223f
Mauro Ciancio <maurociancio@gmail.com>
2010-04-08 00:05:36 -0300

    pagina principal

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/HomePage.java
@@ -0,0 +1,18 @@
+package ar.noxit.ehockey.web.pages;
+
+import java.util.Date;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+
+public class HomePage extends AbstractHeaderPage {
+
+    public HomePage() {
+        add(new Label("titulo", new AbstractReadOnlyModel<String>() {
+
+            @Override
+            public String getObject() {
+                return "Hola, hoy es: " + new Date().toString();
+            }
+        }));
+    }
+}
