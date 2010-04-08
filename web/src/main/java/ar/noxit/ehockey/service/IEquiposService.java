c0261fd3aac8f7a0bdddb2edbd9688ea7c2fa834
Mauro Ciancio <maurociancio@gmail.com>
2010-04-08 01:03:54 -0300

    equipo service
    see #27

---
+++ web/src/main/java/ar/noxit/ehockey/service/IEquiposService.java
@@ -0,0 +1,12 @@
+package ar.noxit.ehockey.service;
+
+import ar.noxit.ehockey.model.Equipo;
+import ar.noxit.exceptions.NoxitException;
+import java.util.List;
+
+public interface IEquiposService {
+
+    List<Equipo> getAll() throws NoxitException;
+
+    Equipo get(Integer id) throws NoxitException;
+}
