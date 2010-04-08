dbb917ad7d6fd0859e423c5dacbcb23f9c5d11ca
Mauro Ciancio <maurociancio@gmail.com>
2010-04-08 01:07:20 -0300

    mostrar listado de equipos en pantalla principal
    see #98

--- web/src/main/java/ar/noxit/ehockey/web/pages/HomePage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/HomePage.java
@@ -1,18 +1,70 @@
 package ar.noxit.ehockey.web.pages;
 
-import java.util.Date;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.model.AbstractReadOnlyModel;
+import ar.noxit.ehockey.model.Equipo;
+import ar.noxit.ehockey.service.IEquiposService;
+import ar.noxit.exceptions.NoxitException;
+import ar.noxit.web.wicket.model.IdLDM;
+import ar.noxit.web.wicket.model.LDM;
+import java.util.List;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 public class HomePage extends AbstractContentPage {
 
-    public HomePage() {
-        add(new Label("titulo", new AbstractReadOnlyModel<String>() {
+    @SpringBean
+    private IEquiposService equiposService;
 
-            @Override
-            public String getObject() {
-                return "Hola, hoy es: " + new Date().toString();
-            }
-        }));
+    public HomePage() {
+        add(new FeedbackPanel("feedback"));
+        Form<Void> form = new Form<Void>("equipos");
+        form.add(new DropDownChoice<Equipo>("equipos",
+                new EquipoModel(new Model<Integer>()),
+                new EquiposListModel(),
+                new EquipoRenderer())
+                .setRequired(true));
+        add(form);
+    }
+
+    private class EquipoRenderer implements IChoiceRenderer<Equipo> {
+
+        @Override
+        public Object getDisplayValue(Equipo object) {
+            return object.getNombre();
+        }
+
+        @Override
+        public String getIdValue(Equipo object, int index) {
+            return object.getId().toString();
+        }
+    }
+
+    private class EquipoModel extends IdLDM<Equipo, Integer> {
+
+        private EquipoModel(IModel<Integer> idModel) {
+            super(idModel);
+        }
+
+        @Override
+        protected Equipo doLoad(Integer id) throws NoxitException {
+            return equiposService.get(id);
+        }
+
+        @Override
+        protected Integer getObjectId(Equipo equipo) {
+            return equipo.getId();
+        }
+    }
+
+    public class EquiposListModel extends LDM<List<Equipo>> {
+
+        @Override
+        protected List<Equipo> doLoad() throws NoxitException {
+            return equiposService.getAll();
+        }
     }
 }
