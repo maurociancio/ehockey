3317d5166d8c16cd0c562fca9aadfc39ebf13bf8
lucasdle <lucasdle@gmail.com>
2010-04-08 00:48:21 -0300

    Test unitarios para la parte del modelo de torneos divisiones y rondas. see #99

---
+++ web/src/test/java/ar/noxit/ehockey/web/model/TestTorneo.java
@@ -0,0 +1,33 @@
+package ar.noxit.ehockey.web.model;
+
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import ar.noxit.ehockey.model.Division;
+import ar.noxit.ehockey.model.Torneo;
+
+public class TestTorneo {
+
+    private Torneo torneo;
+
+    @BeforeMethod
+    public void setUp() {
+        torneo = new Torneo();
+    }
+
+    @Test
+    public void testDivisionExistente() {
+        Division division = new Division("Primera A Hombres");
+        torneo.agregarDivision(division);
+        assertTrue(torneo.existeDivision(division));
+    }
+
+    public void testObtenerDivision() {
+        Division division = new Division("Primera A Mujeres");
+        torneo.agregarDivision(division);
+        assertEquals(division, torneo.getDivision("Primera A Mujeres"));
+    }
+
+}
