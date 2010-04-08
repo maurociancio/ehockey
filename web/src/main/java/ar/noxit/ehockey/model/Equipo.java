4ae9c59cf35e1b785165012c03500df32c2d94df
Mauro Ciancio <maurociancio@gmail.com>
2010-04-08 01:02:53 -0300

    agregar a equipo id
    see #27

--- web/src/main/java/ar/noxit/ehockey/model/Equipo.java
+++ web/src/main/java/ar/noxit/ehockey/model/Equipo.java
@@ -5,6 +5,7 @@ public class Equipo {
     private Club club;
     private ListaBuenaFe listaBuenaFe;
     private String nombre;
+    private Integer id;
 
     public Equipo(String nombre, Club club) {
         this.club = club;
@@ -24,5 +25,38 @@ public class Equipo {
 
     public Club getClub() {
         return club;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Equipo other = (Equipo) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
