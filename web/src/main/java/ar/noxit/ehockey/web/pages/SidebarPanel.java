d5893a6ef106a88261391feabbdde894b2e6aefb
Mauro Ciancio <maurociancio@gmail.com>
2010-04-08 00:10:46 -0300

    sidebar panel

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/SidebarPanel.java
@@ -0,0 +1,10 @@
+package ar.noxit.ehockey.web.pages;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class SidebarPanel extends Panel {
+
+    public SidebarPanel(String id) {
+        super(id);
+    }
+}
