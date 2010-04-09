9653cd78cd153d521c08692c20ebe6dba46c0900
Tito <tito@tito-laptop.(none)>
2010-04-09 01:00:35 -0300

    Excepción para cuando se intenta modificar una planilla que no es modificable. see #37 see #38 see #62

---
+++ web/src/main/java/ar/noxit/ehockey/exception/PlanillaNoModificableException.java
@@ -0,0 +1,31 @@
+package ar.noxit.ehockey.exception;
+
+public class PlanillaNoModificableException extends ReglaNegocioException {
+
+    /**
+     * {@inheritDoc}
+     */
+    public PlanillaNoModificableException() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PlanillaNoModificableException(String message) {
+        super(message);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PlanillaNoModificableException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PlanillaNoModificableException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
