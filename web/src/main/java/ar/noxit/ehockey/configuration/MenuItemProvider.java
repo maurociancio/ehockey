89c07638e60981b769e04a9d0af0c8bed6a39985
Mauro Ciancio <maurociancio@gmail.com>
2010-04-09 17:03:38 -0300

    inyectar los items de la pagina desde spring

--- web/src/main/java/ar/noxit/ehockey/configuration/MenuItemProvider.java
+++ web/src/main/java/ar/noxit/ehockey/configuration/MenuItemProvider.java
@@ -1,21 +1,30 @@
 package ar.noxit.ehockey.configuration;
 
-import ar.noxit.ehockey.web.pages.HomePage;
 import ar.noxit.ehockey.web.pages.IMenuItem;
 import ar.noxit.ehockey.web.pages.IMenuItemProvider;
-import ar.noxit.ehockey.web.pages.ListaBuenaFePage;
-import ar.noxit.ehockey.web.pages.PartidoPage;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.apache.wicket.Page;
 
 public class MenuItemProvider implements IMenuItemProvider {
+
+    private Map<String, Class<? extends Page>> items;
 
     @Override
     public List<IMenuItem> getMenuItems() {
         List<IMenuItem> list = new ArrayList<IMenuItem>();
-        list.add(new MenuItem("HomePage", HomePage.class));
-        list.add(new MenuItem("Lista de Buena Fe", ListaBuenaFePage.class));
-        list.add(new MenuItem("Partidos", PartidoPage.class));
+        Iterator<Entry<String, Class<? extends Page>>> it = items.entrySet().iterator();
+        while (it.hasNext()) {
+            Entry<String, Class<? extends Page>> next = it.next();
+            list.add(new MenuItem(next.getKey(), next.getValue()));
+        }
         return list;
+    }
+
+    public void setItems(Map<String, Class<? extends Page>> items) {
+        this.items = items;
     }
 }
