acb09ef8248791a3f0353e413b70aaf1de51cf6f
Mauro Ciancio <maurociancio@gmail.com>
2010-04-11 17:22:57 -0300

    usar nueva query jugadores

--- web/src/main/java/ar/noxit/ehockey/service/impl/ClubService.java
+++ web/src/main/java/ar/noxit/ehockey/service/impl/ClubService.java
@@ -1,19 +1,20 @@
 package ar.noxit.ehockey.service.impl;
 
 import ar.noxit.ehockey.dao.IClubDao;
+import ar.noxit.ehockey.dao.IJugadorDao;
 import ar.noxit.ehockey.model.Club;
 import ar.noxit.ehockey.model.Equipo;
 import ar.noxit.ehockey.model.Jugador;
 import ar.noxit.ehockey.service.IClubService;
 import ar.noxit.exceptions.NoxitException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import org.springframework.transaction.annotation.Transactional;
 
 public class ClubService implements IClubService {
 
     private IClubDao clubDao;
+    private IJugadorDao jugadorDao;
 
     @Override
     @Transactional(readOnly = true)
@@ -37,17 +38,7 @@ public class ClubService implements IClubService {
     @Override
     @Transactional(readOnly = true)
     public List<Jugador> getJugadoresPorClub(Integer clubId, List<Integer> idJugadores) throws NoxitException {
-        Club club = clubDao.get(clubId);
-
-        List<Jugador> list = new ArrayList<Jugador>(club.getJugadores());
-        Iterator<Jugador> it = list.iterator();
-        while (it.hasNext()) {
-            Jugador next = it.next();
-            if (!idJugadores.contains(next.getFicha())) {
-                it.remove();
-            }
-        }
-        return list;
+        return jugadorDao.getJugadoresFromClub(clubId, idJugadores);
     }
 
     @Override
@@ -59,5 +50,9 @@ public class ClubService implements IClubService {
 
     public void setClubDao(IClubDao clubDao) {
         this.clubDao = clubDao;
+    }
+
+    public void setJugadorDao(IJugadorDao jugadorDao) {
+        this.jugadorDao = jugadorDao;
     }
 }
