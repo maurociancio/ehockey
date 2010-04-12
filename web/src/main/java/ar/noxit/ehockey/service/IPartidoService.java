94932fcf50745748b8a1e9182f89c33f15e77617
lucasdle <lucasdle@gmail.com>
2010-04-11 21:16:05 -0300

    Agregandole funcionalidad al partido service

--- web/src/main/java/ar/noxit/ehockey/service/IPartidoService.java
+++ web/src/main/java/ar/noxit/ehockey/service/IPartidoService.java
@@ -1,5 +1,14 @@
 package ar.noxit.ehockey.service;
 
+import java.util.List;
+
+import ar.noxit.ehockey.model.Partido;
+import ar.noxit.exceptions.NoxitException;
+
 public interface IPartidoService {
+
+    public List<Partido> getAll() throws NoxitException;
+
+    Partido get(Integer id) throws NoxitException;
 
 }
--- web/src/main/java/ar/noxit/ehockey/service/impl/PartidoService.java
+++ web/src/main/java/ar/noxit/ehockey/service/impl/PartidoService.java
@@ -1,7 +1,28 @@
 package ar.noxit.ehockey.service.impl;
 
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import ar.noxit.ehockey.dao.IPartidoDao;
+import ar.noxit.ehockey.model.Equipo;
+import ar.noxit.ehockey.model.Partido;
 import ar.noxit.ehockey.service.IPartidoService;
+import ar.noxit.exceptions.NoxitException;
 
 public class PartidoService implements IPartidoService {
 
+    private IPartidoDao partidoDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Partido> getAll() throws NoxitException {
+        return partidoDao.getAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Partido get(Integer id) throws NoxitException {
+        return partidoDao.get(id);
+    }
 }
