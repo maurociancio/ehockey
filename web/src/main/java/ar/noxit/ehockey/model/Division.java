f99c70fee4b59005349f7011f1f2466c17014047
Mauro Ciancio <maurociancio@gmail.com>
2010-04-15 11:37:59 -0300

    no permitir division nulas
    see #45

--- web/src/main/java/ar/noxit/ehockey/model/Division.java
+++ web/src/main/java/ar/noxit/ehockey/model/Division.java
@@ -1,11 +1,15 @@
 package ar.noxit.ehockey.model;
 
+import org.apache.commons.lang.Validate;
+
 public class Division {
 
     private Integer id;
     private String division;
 
     public Division(String division) {
+        Validate.notNull(division, "division no puede ser null");
+
         this.division = division;
     }
 
