be7c5607d034bb322aeb34be067e51f16d171929
Mauro Ciancio <maurociancio@gmail.com>
2010-04-16 14:23:30 -0300

    sacar hashcode y equals de partido
    see #99

--- web/src/main/java/ar/noxit/ehockey/model/Equipo.java
+++ web/src/main/java/ar/noxit/ehockey/model/Equipo.java
@@ -69,31 +69,6 @@ public class Equipo {
         this.id = id;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Equipo other = (Equipo) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
     protected Equipo() {
     }
 }
