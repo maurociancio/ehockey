69e9a18edbd11aa6025ece683d41372b3508ae03
Tito <tito@tito-laptop.(none)>
2010-04-16 01:55:51 -0300

    Agregando botón de editar a la página de partido
    see #36

--- web/src/main/java/ar/noxit/ehockey/web/pages/partido/PartidoPage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/partido/PartidoPage.java
@@ -7,6 +7,8 @@ import ar.noxit.ehockey.web.pages.planilla.PlanillaPage;
 import ar.noxit.exceptions.NoxitException;
 import ar.noxit.web.wicket.model.LDM;
 import java.util.List;
+
+import org.apache.commons.lang.NotImplementedException;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.IChoiceRenderer;
@@ -14,6 +16,7 @@ import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.markup.html.form.Button;
 
 public class PartidoPage extends AbstractContentPage {
 
@@ -25,16 +28,28 @@ public class PartidoPage extends AbstractContentPage {
         add(new FeedbackPanel("feedback"));
 
         final IModel<Partido> partido = new PartidoModel(new Model<Integer>(), partidoService);
-        Form<Void> formPartidos = new Form<Void>("partidos") {
-
-            @Override
-            protected void onSubmit() {
-                setResponsePage(new PlanillaPage(partido));
-            }
-        };
-
+        Form<Void> formPartidos = new Form<Void>("partidos");
+        
         formPartidos.add(new DropDownChoice<Partido>("partidos", partido, new PartidosListModel(), PARTIDORENDERER)
                 .setRequired(true));
+        
+        formPartidos.add(new Button("ver") {
+
+            @Override
+            public void onSubmit() {
+                setResponsePage(new PlanillaPage(partido));
+            }
+        });
+
+        formPartidos.add(new Button("editar") {
+
+            @Override
+            public void onSubmit() {
+                //TODO implementar esta página
+                throw new NotImplementedException("La página de edición aún no está disponible");
+                //setResponsePage(new PlanillaPage(null));
+            }
+        });
 
         add(formPartidos);
     }
