95285df1ce0fd8be8372635e3193ff5f01c9666a
lucasdle <lucasdle@gmail.com>
2010-04-18 12:58:22 -0300

    Agregado de un datepicker. Cambios en el formulario de alta de jugador, agregado el adaptador de datetime see #29

--- web/src/main/java/ar/noxit/ehockey/web/pages/jugadores/JugadorForm.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/jugadores/JugadorForm.java
@@ -1,8 +1,11 @@
 package ar.noxit.ehockey.web.pages.jugadores;
 
+import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.lang.Validate;
+import org.apache.wicket.extensions.markup.html.form.DateTextField;
+import org.apache.wicket.extensions.yui.calendar.DatePicker;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.RequiredTextField;
@@ -15,6 +18,8 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.LocalTime;
 
 import ar.noxit.ehockey.model.Club;
 import ar.noxit.ehockey.model.Division;
@@ -28,6 +33,7 @@ import ar.noxit.ehockey.web.pages.renderers.DivisionRenderer;
 import ar.noxit.ehockey.web.pages.renderers.SectorRenderer;
 import ar.noxit.exceptions.NoxitException;
 import ar.noxit.exceptions.NoxitRuntimeException;
+import ar.noxit.web.wicket.model.AdapterModel;
 
 public abstract class JugadorForm extends Panel {
 
@@ -58,12 +64,18 @@ public abstract class JugadorForm extends Panel {
                 jugador, "tipoDocumento")));
         form.add(new TextField<String>("numerodoc", new PropertyModel<String>(
                 jugador, "numeroDocumento")));
-        form.add(new TextField<String>("fechanac", new PropertyModel<String>(
-                jugador, "fechaNacimiento")));
+
+        PropertyModel<LocalDate> modelFechaNacimiento = new PropertyModel<LocalDate>(
+                jugador, "fechaNacimiento");
+        DateTextField fechaEvento = new DateTextField("fechanac",
+                new DateAdapter(modelFechaNacimiento), "dd/MM/yyyy");
+        fechaEvento.add(new DatePicker());
+        form.add(fechaEvento);
+
         form.add(new TextField<String>("telefono", new PropertyModel<String>(
                 jugador, "telefono")));
         form.add(new TextField<String>("fechaalta", new Model<String>(
-                new LocalDate().toString("dd-MM-yyyy"))).setEnabled(false));
+                new LocalDate().toString("dd/MM/yyyy"))).setEnabled(false));
         form.add(new DropDownChoice<Club>("club", new PropertyModel<Club>(
                 jugador, "club"), new ClubListModel(clubService),
                 new ClubRenderer()).setRequired(true));
@@ -98,7 +110,7 @@ public abstract class JugadorForm extends Panel {
             try {
                 return clubService.getAll();
             } catch (NoxitException ex) {
-                // @todo
+                // #TODO
                 throw new NoxitRuntimeException(ex);
             }
         }
@@ -120,7 +132,7 @@ public abstract class JugadorForm extends Panel {
             try {
                 return divisionService.getAll();
             } catch (NoxitException ex) {
-                // @todo
+                // #TODO
                 throw new NoxitRuntimeException(ex);
             }
         }
@@ -141,10 +153,32 @@ public abstract class JugadorForm extends Panel {
             try {
                 return sectorService.getAll();
             } catch (NoxitException ex) {
-                // @todo
+                // #TODO
                 throw new NoxitRuntimeException(ex);
             }
         }
     }
 
+    private class DateAdapter extends AdapterModel<Date, LocalDate> {
+
+        public DateAdapter(IModel<LocalDate> delegate) {
+            super(delegate);
+        }
+
+        @SuppressWarnings("deprecation")
+        @Override
+        protected Date getObject(IModel<LocalDate> delegate) {
+            LocalDate fecha = delegate.getObject();
+            if (fecha == null)
+                return null;
+            return new Date(fecha.getYear(), fecha.getMonthOfYear(), fecha
+                    .getDayOfMonth());
+        }
+
+        @Override
+        protected void setObject(Date object, IModel<LocalDate> delegate) {
+            LocalDate temp = delegate.getObject();
+            delegate.setObject(new LocalDate(object));
+        }
+    }
 }
