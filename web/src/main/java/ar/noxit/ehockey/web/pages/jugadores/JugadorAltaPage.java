489d2316b15f3467b4c09635e27d2e5172feeb8d
lucasdle <lucasdle@gmail.com>
2010-04-18 02:36:46 -0300

    Pagina de agregar jugador sin estilos. see #29

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/jugadores/JugadorAltaPage.java
@@ -0,0 +1,39 @@
+package ar.noxit.ehockey.web.pages.jugadores;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import ar.noxit.ehockey.model.Jugador;
+import ar.noxit.ehockey.service.IJugadorService;
+import ar.noxit.ehockey.web.pages.base.AbstractContentPage;
+import ar.noxit.exceptions.NoxitException;
+
+public class JugadorAltaPage extends AbstractContentPage {
+
+    @SpringBean
+    private IJugadorService jugadorService;
+
+    public JugadorAltaPage() {
+        IModel<Jugador> jugador = new Model<Jugador>(new Jugador());
+
+        // creamos un formulario
+        add(new JugadorForm("formulario", jugador) {
+            @Override
+            protected void onSubmit(IModel<Jugador> jugador) {
+                try {
+                    // tratamos de agregar a la persona
+                    jugadorService.add(jugador.getObject());
+                    info("El jugador " + jugador.getObject().getApellido()
+                            + ", " + jugador.getObject().getNombre()
+                            + " ha sido agregada correctamente.");
+                    jugador.setObject(new Jugador());
+                } catch (NoxitException ex) {
+                    // se produjo una excepcion
+                    // la levantamos pero en runtime
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
+}
---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/jugadores/JugadorForm.java
@@ -0,0 +1,169 @@
+package ar.noxit.ehockey.web.pages.jugadores;
+
+import java.util.List;
+
+import org.apache.commons.lang.Validate;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import ar.noxit.ehockey.model.Club;
+import ar.noxit.ehockey.model.Division;
+import ar.noxit.ehockey.model.Jugador;
+import ar.noxit.ehockey.model.Sector;
+import ar.noxit.ehockey.service.IClubService;
+import ar.noxit.ehockey.service.IDivisionService;
+import ar.noxit.ehockey.service.ISectorService;
+import ar.noxit.exceptions.NoxitException;
+import ar.noxit.exceptions.NoxitRuntimeException;
+
+public abstract class JugadorForm extends Panel {
+
+    @SpringBean
+    private IClubService clubService;
+    @SpringBean
+    private IDivisionService divisionService;
+    @SpringBean
+    private ISectorService sectorService;
+
+    public JugadorForm(String id, final IModel<Jugador> jugador) {
+        super(id);
+        Validate.notNull(jugador);
+
+        Form<Jugador> form = new Form<Jugador>("agregar_jugador", jugador) {
+
+            @Override
+            protected void onSubmit() {
+                JugadorForm.this.onSubmit(jugador);
+            }
+        };
+
+        form.add(new RequiredTextField<String>("nombre",
+                new PropertyModel<String>(jugador, "nombre")));
+        form.add(new RequiredTextField<String>("apellido",
+                new PropertyModel<String>(jugador, "apellido")));
+        form.add(new DropDownChoice<Club>("club", new PropertyModel<Club>(
+                jugador, "club"), new ClubListModel(clubService),
+                new ClubRenderer()).setRequired(true));
+        form.add(new DropDownChoice<Division>("division",
+                new PropertyModel<Division>(jugador, "division"),
+                new DivisionListModel(divisionService), new DivisionRenderer())
+                .setRequired(true));
+        form.add(new DropDownChoice<Sector>("sector",
+                new PropertyModel<Sector>(jugador, "sector"),
+                new SectorListModel(sectorService), new SectorRenderer())
+                .setRequired(true));
+        add(form);
+        add(new FeedbackPanel("feedback"));
+    }
+
+    protected abstract void onSubmit(IModel<Jugador> jugador);
+
+    private class ClubListModel extends LoadableDetachableModel<List<Club>> {
+
+        private IClubService clubService;
+
+        public ClubListModel(IClubService jugadorService) {
+            Validate.notNull(jugadorService);
+
+            this.clubService = jugadorService;
+        }
+
+        @Override
+        protected List<Club> load() {
+            try {
+                return clubService.getAll();
+            } catch (NoxitException ex) {
+                // @todo
+                throw new NoxitRuntimeException(ex);
+            }
+        }
+    }
+
+    private final class ClubRenderer implements IChoiceRenderer<Club> {
+        @Override
+        public Object getDisplayValue(Club arg0) {
+            return arg0.getNombre();
+        }
+
+        @Override
+        public String getIdValue(Club arg0, int arg1) {
+            return arg0.getId().toString();
+        }
+    }
+
+    private class DivisionListModel extends
+            LoadableDetachableModel<List<Division>> {
+
+        private IDivisionService divisionService;
+
+        public DivisionListModel(IDivisionService divisionService) {
+            Validate.notNull(divisionService);
+
+            this.divisionService = divisionService;
+        }
+
+        @Override
+        protected List<Division> load() {
+            try {
+                return divisionService.getAll();
+            } catch (NoxitException ex) {
+                // @todo
+                throw new NoxitRuntimeException(ex);
+            }
+        }
+    }
+
+    public final class DivisionRenderer implements IChoiceRenderer<Division> {
+        @Override
+        public Object getDisplayValue(Division arg0) {
+            return arg0.getNombre();
+        }
+
+        @Override
+        public String getIdValue(Division arg0, int arg1) {
+            return arg0.getId().toString();
+        }
+    }
+
+    private class SectorListModel extends LoadableDetachableModel<List<Sector>> {
+
+        private ISectorService sectorService;
+
+        public SectorListModel(ISectorService sectorService) {
+            Validate.notNull(sectorService);
+
+            this.sectorService = sectorService;
+        }
+
+        @Override
+        protected List<Sector> load() {
+            try {
+                return sectorService.getAll();
+            } catch (NoxitException ex) {
+                // @todo
+                throw new NoxitRuntimeException(ex);
+            }
+        }
+    }
+
+    public final class SectorRenderer implements IChoiceRenderer<Sector> {
+        @Override
+        public Object getDisplayValue(Sector arg0) {
+            return arg0.getSector();
+        }
+
+        @Override
+        public String getIdValue(Sector arg0, int arg1) {
+            return arg0.getId().toString();
+        }
+    }
+
+}
