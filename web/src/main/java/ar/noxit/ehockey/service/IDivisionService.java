5c885b1f4ee85b71c781fe81c2e39bfe81004091
lucasdle <lucasdle@gmail.com>
2010-04-19 00:16:58 -0300

    Division service actualizado see #29 see #31

--- web/src/main/java/ar/noxit/ehockey/service/IDivisionService.java
+++ web/src/main/java/ar/noxit/ehockey/service/IDivisionService.java
@@ -3,10 +3,13 @@ package ar.noxit.ehockey.service;
 import java.util.List;
 
 import ar.noxit.ehockey.model.Division;
+import ar.noxit.ehockey.web.pages.jugadores.DivisionPlano;
 import ar.noxit.exceptions.NoxitException;
 
 public interface IDivisionService {
 
     List<Division> getAll() throws NoxitException;
+
+    public List<DivisionPlano> getAllPlano() throws NoxitException;
 
 }
--- web/src/main/java/ar/noxit/ehockey/service/impl/DivisionService.java
+++ web/src/main/java/ar/noxit/ehockey/service/impl/DivisionService.java
@@ -1,19 +1,40 @@
 package ar.noxit.ehockey.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
 
 import ar.noxit.ehockey.dao.IDivisionDao;
 import ar.noxit.ehockey.model.Division;
 import ar.noxit.ehockey.service.IDivisionService;
+import ar.noxit.ehockey.web.pages.jugadores.DivisionPlano;
 import ar.noxit.exceptions.NoxitException;
+import ar.noxit.exceptions.persistence.PersistenceException;
 
 public class DivisionService implements IDivisionService {
 
     private IDivisionDao divisionDao;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Division> getAll() throws NoxitException {
         return divisionDao.getAll();
+    }
+
+    @Override
+    public List<DivisionPlano> getAllPlano() throws NoxitException {
+        List<DivisionPlano> divisiones = new ArrayList<DivisionPlano>();
+        for (Division each : divisionDao.getAll()) {
+            divisiones.add(aplanar(each));
+        }
+        return divisiones;
+    }
+
+    private DivisionPlano aplanar(Division division) {
+        DivisionPlano div = new DivisionPlano();
+        div.setDivision(division.getNombre());
+        return div;
     }
 
     public void setDivisionDao(IDivisionDao divisionDao) {
