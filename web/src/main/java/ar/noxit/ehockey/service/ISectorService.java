77e5671d2b60858231e79573af8b54e3fc0ba8f2
lucasdle <lucasdle@gmail.com>
2010-04-19 00:19:43 -0300

    Actualizacion del sector service see #29 see #31

--- web/src/main/java/ar/noxit/ehockey/service/ISectorService.java
+++ web/src/main/java/ar/noxit/ehockey/service/ISectorService.java
@@ -3,10 +3,13 @@ package ar.noxit.ehockey.service;
 import java.util.List;
 
 import ar.noxit.ehockey.model.Sector;
+import ar.noxit.ehockey.web.pages.jugadores.SectorPlano;
 import ar.noxit.exceptions.NoxitException;
 
 public interface ISectorService {
 
     public List<Sector> getAll() throws NoxitException;
+
+    public List<SectorPlano> getAllPlano() throws NoxitException;
 
 }
--- web/src/main/java/ar/noxit/ehockey/service/impl/SectorService.java
+++ web/src/main/java/ar/noxit/ehockey/service/impl/SectorService.java
@@ -1,10 +1,12 @@
 package ar.noxit.ehockey.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ar.noxit.ehockey.dao.ISectorDao;
 import ar.noxit.ehockey.model.Sector;
 import ar.noxit.ehockey.service.ISectorService;
+import ar.noxit.ehockey.web.pages.jugadores.SectorPlano;
 import ar.noxit.exceptions.NoxitException;
 
 public class SectorService implements ISectorService {
@@ -14,6 +16,21 @@ public class SectorService implements ISectorService {
     @Override
     public List<Sector> getAll() throws NoxitException {
         return sectorDao.getAll();
+    }
+
+    @Override
+    public List<SectorPlano> getAllPlano() throws NoxitException {
+        List<SectorPlano> sectores = new ArrayList<SectorPlano>();
+        for (Sector each : sectorDao.getAll()) {
+            sectores.add(aplanar(each));
+        }
+        return sectores;
+    }
+
+    private SectorPlano aplanar(Sector sector) {
+        SectorPlano sec = new SectorPlano();
+        sec.setSector(sector.getSector());
+        return sec;
     }
 
     public void setSectorDao(ISectorDao sectorDao) {
