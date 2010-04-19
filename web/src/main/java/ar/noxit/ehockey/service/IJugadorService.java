8807475698d2f0b13fdc007ba4c22c981f8a07b2
lucasdle <lucasdle@gmail.com>
2010-04-19 00:19:10 -0300

    Actualizacion del jugador service see #29 see #31

--- web/src/main/java/ar/noxit/ehockey/service/IJugadorService.java
+++ web/src/main/java/ar/noxit/ehockey/service/IJugadorService.java
@@ -3,17 +3,22 @@ package ar.noxit.ehockey.service;
 import java.util.List;
 
 import ar.noxit.ehockey.model.Jugador;
+import ar.noxit.ehockey.web.pages.jugadores.JugadorPlano;
 import ar.noxit.exceptions.NoxitException;
 
 public interface IJugadorService {
-    
+
     public void add(Jugador jugador) throws NoxitException;
-    
+
     public void update(Jugador jugador) throws NoxitException;
-    
+
     public void remove(Jugador jugador) throws NoxitException;
 
     public List<Jugador> getAll() throws NoxitException;
 
-    Jugador get(Integer id) throws NoxitException;
+    public Jugador get(Integer id) throws NoxitException;
+
+    public void convertAdd(JugadorPlano jugador) throws NoxitException;
+
+    public List<JugadorPlano> getAllPlano() throws NoxitException;
 }
--- web/src/main/java/ar/noxit/ehockey/service/impl/JugadorService.java
+++ web/src/main/java/ar/noxit/ehockey/service/impl/JugadorService.java
@@ -1,13 +1,23 @@
 package ar.noxit.ehockey.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.transaction.annotation.Transactional;
 
 import ar.noxit.ehockey.dao.IJugadorDao;
+import ar.noxit.ehockey.exception.SinClubException;
+import ar.noxit.ehockey.model.Club;
+import ar.noxit.ehockey.model.Division;
 import ar.noxit.ehockey.model.Jugador;
+import ar.noxit.ehockey.model.Sector;
 import ar.noxit.ehockey.service.IJugadorService;
+import ar.noxit.ehockey.web.pages.jugadores.ClubPlano;
+import ar.noxit.ehockey.web.pages.jugadores.DivisionPlano;
+import ar.noxit.ehockey.web.pages.jugadores.JugadorPlano;
+import ar.noxit.ehockey.web.pages.jugadores.SectorPlano;
 import ar.noxit.exceptions.NoxitException;
+import ar.noxit.exceptions.NoxitRuntimeException;
 
 public class JugadorService implements IJugadorService {
 
@@ -50,6 +60,59 @@ public class JugadorService implements IJugadorService {
     @Transactional(rollbackFor = { RuntimeException.class, NoxitException.class })
     public List<Jugador> getAll() throws NoxitException {
         return jugadorDao.getAll();
+    }
+
+    @Override
+    @Transactional(rollbackFor = { RuntimeException.class, NoxitException.class })
+    public void convertAdd(JugadorPlano jugador) throws NoxitException {
+        Jugador jdr = assembly(jugador);
+        jugadorDao.save(jdr);
+
+    }
+
+    @Override
+    @Transactional(rollbackFor = { RuntimeException.class, NoxitException.class })
+    public List<JugadorPlano> getAllPlano() throws NoxitException {
+        List<JugadorPlano> jugadores = new ArrayList<JugadorPlano>();
+        for (Jugador each : jugadorDao.getAll()) {
+            jugadores.add(aplanar(each));
+        }
+        return jugadores;
+    }
+
+    private Jugador assembly(JugadorPlano jugador) {
+        Jugador jdr = new Jugador(jugador.getApellido(), jugador.getNombre(),
+                new Club(jugador.getClub().getNombre()), new Sector(jugador
+                        .getSector().getSector()), new Division(jugador
+                        .getDivision().getDivision()));
+        jdr.setFechaAlta(jugador.getFechaAlta());
+        jdr.setFechaNacimiento(jugador.getFechaNacimiento());
+        jdr.setLetraJugador(jugador.getLetraJugador());
+        jdr.setNumeroDocumento(jugador.getNumeroDocumento());
+        jdr.setTipoDocumento(jugador.getTipoDocumento());
+        jdr.setTelefono(jugador.getTelefono());
+        return jdr;
+    }
+
+    private JugadorPlano aplanar(Jugador jugador) {
+        JugadorPlano jdr = new JugadorPlano();
+        jdr.setApellido(jugador.getApellido());
+        try {
+            jdr.setClub(jugador.getClub());
+        } catch (SinClubException e) {
+            throw new NoxitRuntimeException(e);
+        }
+        jdr.setDivision(jugador.getDivision());
+        jdr.setFechaAlta(jugador.getFechaAlta());
+        jdr.setFechaNacimiento(jugador.getFechaNacimiento());
+        jdr.setFicha(jugador.getFicha());
+        jdr.setLetraJugador(jugador.getLetraJugador());
+        jdr.setNombre(jugador.getNombre());
+        jdr.setNumeroDocumento(jugador.getDocumento());
+        jdr.setSector(jugador.getSector());
+        jdr.setTelefono(jugador.getTelefono());
+        jdr.setTipoDocumento(jugador.getTipoDocumento());
+        return jdr;
     }
 
     public void setJugadorDao(IJugadorDao jugadorDao) {
