a949195942fa7ea04bc0d4fb01ed7b7e7243bb6f
Mauro Ciancio <maurociancio@gmail.com>
2010-04-22 18:21:52 -0300

    partido terminado exception

---
+++ web/src/main/java/ar/noxit/ehockey/exception/PartidoYaTerminadoException.java
@@ -0,0 +1,38 @@
+package ar.noxit.ehockey.exception;
+
+/**
+ * Lanzada cuando el partido ya está finalizado y se intenta finalizar
+ * nuevamente
+ * 
+ * @author Mauro Ciancio
+ * 
+ */
+public class PartidoYaTerminadoException extends ReglaNegocioException {
+
+    /**
+     * {@inheritDoc}
+     */
+    public PartidoYaTerminadoException() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PartidoYaTerminadoException(String message) {
+        super(message);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PartidoYaTerminadoException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PartidoYaTerminadoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
