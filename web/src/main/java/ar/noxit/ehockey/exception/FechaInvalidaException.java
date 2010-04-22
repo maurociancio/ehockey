2a92ac7ba68fd484546774fb6392c6e94c6ae47d
Mauro Ciancio <maurociancio@gmail.com>
2010-04-22 18:21:42 -0300

    fecha invalida exception

---
+++ web/src/main/java/ar/noxit/ehockey/exception/FechaInvalidaException.java
@@ -0,0 +1,37 @@
+package ar.noxit.ehockey.exception;
+
+/**
+ * Lanzada cuando la fecha de inicio del partido es invalida
+ * 
+ * @author Mauro Ciancio
+ * 
+ */
+public class FechaInvalidaException extends ReglaNegocioException {
+
+    /**
+     * {@inheritDoc}
+     */
+    public FechaInvalidaException() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public FechaInvalidaException(String message) {
+        super(message);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public FechaInvalidaException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public FechaInvalidaException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
