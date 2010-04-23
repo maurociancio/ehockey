aef76fab4518ff1d0f656b0e7bb53690e70ead68
Mauro Ciancio <maurociancio@gmail.com>
2010-04-23 00:12:55 -0300

    ver partidos page
    see #87

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/torneo/VerPartidosPage.java
@@ -0,0 +1,10 @@
+package ar.noxit.ehockey.web.pages.torneo;
+
+import ar.noxit.ehockey.model.Torneo;
+import org.apache.wicket.model.IModel;
+
+public class VerPartidosPage extends BaseTorneoPage {
+
+    public VerPartidosPage(IModel<Torneo> torneo) {
+    }
+}
