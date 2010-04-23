db7888599aa1e50d3dcecff66479eb0a6a1f4261
Mauro Ciancio <maurociancio@gmail.com>
2010-04-22 23:29:59 -0300

    accion panel en listado de torneos
    see #87

--- web/src/main/java/ar/noxit/ehockey/web/pages/torneo/ListadoTorneoPage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/torneo/ListadoTorneoPage.java
@@ -7,9 +7,13 @@ import ar.noxit.web.wicket.model.LDM;
 import ar.noxit.web.wicket.provider.SimpleDataProvider;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
@@ -24,8 +28,22 @@ public class ListadoTorneoPage extends BaseTorneoPage {
 
         columns.add(new PropertyColumn<Torneo>(Model.of("Nombre de Torneo"), "nombre"));
         columns.add(new PropertyColumn<Torneo>(Model.of("Cantidad de Partidos"), "cantidadPartidos"));
+        columns.add(new AbstractColumn<Torneo>(Model.of("Acciones")) {
+
+            @Override
+            public void populateItem(Item<ICellPopulator<Torneo>> cellItem, String componentId, IModel<Torneo> rowModel) {
+                cellItem.add(new ActionPanel(componentId, rowModel));
+            }
+        });
 
         add(new DefaultDataTable<Torneo>("listado", columns, new TorneosDataProvider(), 20));
+    }
+
+    private class ActionPanel extends Panel {
+
+        public ActionPanel(String id, IModel<Torneo> torneo) {
+            super(id);
+        }
     }
 
     private class TorneosDataProvider extends SimpleDataProvider<Torneo> {
