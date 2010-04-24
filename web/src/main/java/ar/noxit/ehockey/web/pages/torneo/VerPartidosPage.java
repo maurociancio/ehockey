43907c10d30fdcd90e65707c32e384f6e14e71ae
Mauro Ciancio <maurociancio@gmail.com>
2010-04-24 16:42:14 -0300

    listar partidos de un torneo
    see #87

--- web/src/main/java/ar/noxit/ehockey/web/pages/torneo/VerPartidosPage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/torneo/VerPartidosPage.java
@@ -1,10 +1,71 @@
 package ar.noxit.ehockey.web.pages.torneo;
 
+import ar.noxit.ehockey.model.Partido;
 import ar.noxit.ehockey.model.Torneo;
+import ar.noxit.exceptions.NoxitException;
+import ar.noxit.utils.Collections;
+import ar.noxit.web.wicket.column.AbstractReadOnlyColumn;
+import ar.noxit.web.wicket.model.LDM;
+import ar.noxit.web.wicket.provider.DataProvider;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang.Validate;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
 public class VerPartidosPage extends BaseTorneoPage {
 
     public VerPartidosPage(IModel<Torneo> torneo) {
+        Validate.notNull(torneo, "torneo no puede ser null");
+
+        List<IColumn<Partido>> columns = new ArrayList<IColumn<Partido>>();
+        columns.add(new PropertyColumn<Partido>(Model.of("Local"), "local.nombre"));
+        columns.add(new PropertyColumn<Partido>(Model.of("Visitante"), "visitante.nombre"));
+        columns.add(new AbstractReadOnlyColumn<Partido>(Model.of("Jugado?")) {
+
+            @Override
+            protected String getLabelString(Partido object) {
+                return object.isJugado() ? "Si" : "No";
+            }
+        });
+
+        add(new DefaultDataTable<Partido>("partidos", columns, new PartidosFromTorneoDataProvider(torneo), 20));
+    }
+
+    private final class PartidosFromTorneoDataProvider extends DataProvider<Partido> {
+
+        private IModel<Torneo> torneo;
+
+        public PartidosFromTorneoDataProvider(IModel<Torneo> torneo) {
+            Validate.notNull(torneo);
+            this.torneo = torneo;
+        }
+
+        @Override
+        protected List<Partido> loadList() {
+            Torneo t = torneo.getObject();
+            return Collections.toList(t.iteradorPartidos());
+        }
+
+        @Override
+        public IModel<Partido> model(Partido object) {
+            final Integer id = object.getId();
+            return new LDM<Partido>(object) {
+
+                @Override
+                protected Partido doLoad() throws NoxitException {
+                    List<Partido> partidos = loadList();
+                    for (Partido p : partidos) {
+                        if (p.getId().equals(id)) {
+                            return p;
+                        }
+                    }
+                    return null;
+                }
+            };
+        }
     }
 }
