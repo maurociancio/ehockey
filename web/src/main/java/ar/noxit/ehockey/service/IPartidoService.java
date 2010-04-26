b77383cad15fc72c961c796ea803e9cbdfae578c
Mauro Ciancio <maurociancio@gmail.com>
2010-04-26 00:16:51 -0300

    reprogramacion service
    see #87

--- web/src/main/java/ar/noxit/ehockey/service/IPartidoService.java
+++ web/src/main/java/ar/noxit/ehockey/service/IPartidoService.java
@@ -1,13 +1,15 @@
 package ar.noxit.ehockey.service;
 
-import java.util.List;
-
 import ar.noxit.ehockey.model.Partido;
+import ar.noxit.ehockey.service.transfer.PartidoInfo;
 import ar.noxit.exceptions.NoxitException;
+import java.util.List;
 
 public interface IPartidoService {
 
     public List<Partido> getAll() throws NoxitException;
 
     Partido get(Integer id) throws NoxitException;
+
+    void reprogramar(PartidoInfo pi) throws NoxitException;
 }
