b622cad6049ad25ea05cd06ca76c975e2e548931
Mauro Ciancio <maurociancio@gmail.com>
2010-04-26 10:27:25 -0300

    montar urls

--- web/src/main/java/ar/noxit/ehockey/web/app/EHockeyApplication.java
+++ web/src/main/java/ar/noxit/ehockey/web/app/EHockeyApplication.java
@@ -4,12 +4,19 @@ import ar.noxit.ehockey.main.StartJetty;
 import ar.noxit.ehockey.web.pages.HomePage;
 import ar.noxit.ehockey.web.pages.buenafe.EditarListaBuenaFePage;
 import ar.noxit.ehockey.web.pages.buenafe.VerListaBuenaFePage;
+import ar.noxit.ehockey.web.pages.jugadores.JugadorAltaPage;
+import ar.noxit.ehockey.web.pages.jugadores.JugadorBajaPage;
+import ar.noxit.ehockey.web.pages.jugadores.JugadorModificarPage;
+import ar.noxit.ehockey.web.pages.jugadores.JugadorPage;
+import ar.noxit.ehockey.web.pages.jugadores.JugadorVerPage;
 import ar.noxit.ehockey.web.pages.partido.PartidoPage;
 import ar.noxit.ehockey.web.pages.planilla.PlanillaPage;
 import ar.noxit.ehockey.web.pages.planilla.PlanillaPrinterFriendly;
 import ar.noxit.ehockey.web.pages.torneo.ListadoTorneoPage;
 import ar.noxit.ehockey.web.pages.torneo.NuevoTorneoPage;
+import ar.noxit.ehockey.web.pages.torneo.ReprogramacionPartidoPage;
 import ar.noxit.ehockey.web.pages.torneo.TorneoPage;
+import ar.noxit.ehockey.web.pages.torneo.VerPartidosPage;
 import org.apache.commons.lang.Validate;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
@@ -50,6 +57,13 @@ public class EHockeyApplication extends WebApplication {
         mount(new HybridUrlCodingStrategy("/torneos", TorneoPage.class, false));
         mount(new HybridUrlCodingStrategy("/torneos/crear", NuevoTorneoPage.class, false));
         mount(new HybridUrlCodingStrategy("/torneos/listado", ListadoTorneoPage.class, false));
+        mount(new HybridUrlCodingStrategy("/torneos/ver", VerPartidosPage.class, false));
+        mount(new HybridUrlCodingStrategy("/partidos/reprogramar", ReprogramacionPartidoPage.class, false));
+        mount(new HybridUrlCodingStrategy("/jugadores", JugadorPage.class, false));
+        mount(new HybridUrlCodingStrategy("/jugadores/alta", JugadorAltaPage.class, false));
+        mount(new HybridUrlCodingStrategy("/jugadores/baja", JugadorBajaPage.class, false));
+        mount(new HybridUrlCodingStrategy("/jugadores/modificar", JugadorModificarPage.class, false));
+        mount(new HybridUrlCodingStrategy("/jugadores/ver", JugadorVerPage.class, false));
     }
 
     /**
