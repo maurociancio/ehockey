07bbee712785fd1dc972fad23037a7cb00c0829d
Mauro Ciancio <maurociancio@gmail.com>
2010-04-29 17:55:39 -0300

    modelo para los equipos de un sector y de una division
    see #132
    Time: 0h10m

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/models/EquiposDeSectorYDivisionModel.java
@@ -0,0 +1,30 @@
+package ar.noxit.ehockey.web.pages.models;
+
+import ar.noxit.ehockey.model.Equipo;
+import ar.noxit.ehockey.service.IEquiposService;
+import ar.noxit.exceptions.NoxitException;
+import java.util.List;
+import org.apache.wicket.model.IModel;
+
+public class EquiposDeSectorYDivisionModel extends AbstractListEquipoModel {
+
+    private IModel<Integer> sectorId;
+    private IModel<Integer> divisionId;
+
+    public EquiposDeSectorYDivisionModel(IEquiposService equiposService, IModel<Integer> sectorId,
+            IModel<Integer> divisionId) {
+        super(equiposService);
+
+        this.sectorId = sectorId;
+        this.divisionId = divisionId;
+    }
+
+    @Override
+    protected List<Equipo> doLoad() throws NoxitException {
+        Integer sector = sectorId.getObject();
+        Integer division = divisionId.getObject();
+
+        // #TODO implementar
+        return this.equiposService.getAll();
+    }
+}
