f11d4850cabc09df56e41e10adc1c5ee344e6a39
Mauro Ciancio <maurociancio@gmail.com>
2010-04-30 13:11:45 -0300

    implementar en equipos dao para buscar equipos con un club y una division determinada
    see #132
    Time: 0h15m

--- web/src/main/java/ar/noxit/ehockey/dao/IEquipoDao.java
+++ web/src/main/java/ar/noxit/ehockey/dao/IEquipoDao.java
@@ -2,6 +2,10 @@ package ar.noxit.ehockey.dao;
 
 import ar.noxit.dataaccessobject.IDao;
 import ar.noxit.ehockey.model.Equipo;
+import ar.noxit.exceptions.persistence.PersistenceException;
+import java.util.List;
 
 public interface IEquipoDao extends IDao<Equipo, Integer> {
+
+    List<Equipo> getEquiposDe(Integer sector, Integer division) throws PersistenceException;
 }
--- web/src/main/java/ar/noxit/ehockey/dao/impl/EquipoDao.java
+++ web/src/main/java/ar/noxit/ehockey/dao/impl/EquipoDao.java
@@ -3,10 +3,23 @@ package ar.noxit.ehockey.dao.impl;
 import ar.noxit.dataaccessobject.hibernate.HibernateDao;
 import ar.noxit.ehockey.dao.IEquipoDao;
 import ar.noxit.ehockey.model.Equipo;
+import ar.noxit.exceptions.persistence.PersistenceException;
+import java.util.List;
+import org.hibernate.Session;
 
 public class EquipoDao extends HibernateDao<Equipo, Integer> implements IEquipoDao {
 
     public EquipoDao() {
         super(Equipo.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Equipo> getEquiposDe(Integer sector, Integer division) throws PersistenceException {
+        Session session = getSession();
+        return session.createQuery("from Equipo e where e.division.id = :division and e.sector.id = :sector")
+                .setParameter("division", division)
+                .setParameter("sector", sector)
+                .list();
     }
 }
