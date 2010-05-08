a6248b715862de00d56043b067fd49bd5147746a
lucasdle <lucasdle@gmail.com>
2010-05-08 01:00:46 -0300

    Clases utilitarias para tunear el formulario de ingreso de jugador. see #128

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/jugadores/FeedBackLabelAttachedComponent.java
@@ -0,0 +1,35 @@
+package ar.noxit.ehockey.web.pages.jugadores;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.markup.html.panel.Fragment;
+import org.apache.wicket.model.Model;
+
+public class FeedBackLabelAttachedComponent<T extends FormComponent<?>> extends
+        Fragment {
+
+    private FeedbackLabel label;
+
+    public FeedBackLabelAttachedComponent(String fragmentid,
+            String fragmentComponentid, String feedbackid, T component,
+            MarkupContainer container) {
+        this(fragmentid, fragmentComponentid, feedbackid, component, container,
+                "onblur");
+    }
+
+    public FeedBackLabelAttachedComponent(String fragmentid,
+            String fragmentComponentid, String feedbackid, T component,
+            MarkupContainer container, String event) {
+        super(fragmentid, fragmentComponentid, container);
+        label = new FeedbackLabel(feedbackid, component);
+        component.add(new ComponentVisualErrorBehavior(event, label))
+                .setOutputMarkupId(true);
+        label.setOutputMarkupId(true);
+        this.add(component);
+        this.add(label);
+    }
+
+}
