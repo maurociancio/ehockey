928d5774e3d3b5577a4654d729527a6351000494
Tito <tito@tito-laptop.(none)>
2010-05-09 18:35:31 -0300

    Cambiando la vista de planilla. see #60

--- web/src/main/java/ar/noxit/ehockey/web/pages/planilla/PlanillaPanel.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/planilla/PlanillaPanel.java
@@ -11,32 +11,38 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.PropertyModel;
 import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import ar.noxit.ehockey.model.Jugador;
 import ar.noxit.ehockey.model.Planilla;
+import ar.noxit.web.wicket.model.AbstractLocalDateTimeFormatModel;
 
 public class PlanillaPanel extends Panel {
 
     private String FEDERACION = "Federación de Hockey - FIUBA - 75.47";
 
-    public PlanillaPanel(String id, IModel<? extends Planilla> planillaModel) {
+    public PlanillaPanel(String id, IModel<Planilla> planillaModel) {
         super(id);
 
         add(new Label("federacion", this.FEDERACION));
-        add(new Label("torneo", "Torneo"));
-        add(new Label("rueda", "Rueda"));
-        add(new Label("fecha", "Fecha"));
-        add(new Label("partido", "Partido"));
-        add(new Label("sector", "Sector"));
-        add(new Label("categoria", "Categoria"));
-        add(new Label("division", "Division"));
-        add(new Label("zona", "Zona"));
-        add(new Label("dia", new LocalDateTime().dayOfMonth().getAsShortText()));
-        add(new Label("mes", new LocalDateTime().monthOfYear().getAsShortText()));
-        add(new Label("año", new LocalDateTime().year().getAsShortText()));
+        add(new Label("torneo", new PropertyModel<String>(planillaModel, "partido.torneo.nombre")));
+        add(new Label("rueda", new PropertyModel<Integer>(planillaModel, "partido.rueda")));
+        add(new Label("fecha", new PropertyModel<Integer>(planillaModel, "partido.fechaDelTorneo")));
+        add(new Label("partido", "Partido 1")); // TODO GUARDAR NUMERO DE
+                                                // PARTIDO
+        add(new Label("sector", "Sector")); // TODO GUARDAR EL SECTOR EN TORNEO
+        add(new Label("categoria", "Campeonato"));
+        add(new Label("division", "Division")); // TODO GUARDAR LA DIVISION EN
+                                                // EL TORNEO
+        add(new Label("zona", ""));
+        IModel<LocalDateTime> modelTime = new PropertyModel<LocalDateTime>(planillaModel, "partido.inicio");
+        add(new Label("dia", new DiaAdapterModel(modelTime)));
+        add(new Label("mes", new MesAdapterModel(modelTime)));
+        add(new Label("año", new AnoAdapterModel(modelTime)));
         add(new Label("lugar", "Paseo Colón"));
-        add(new Label("nombreLocal", planillaModel.getObject().getLocal().getNombre()));
-        add(new Label("nombreVisitante", planillaModel.getObject().getVisitante().getNombre()));
+        add(new Label("nombreLocal", new PropertyModel<String>(planillaModel, "local.nombre")));
+        add(new Label("nombreVisitante", new PropertyModel<String>(planillaModel, "visitante.nombre")));
 
         IModel<List<Jugador>> modelLocal = new JugadorLocalModelItem(planillaModel);
         IModel<List<Jugador>> modelVisitante = new JugadorVisitanteModelItem(planillaModel);
@@ -119,5 +125,44 @@ public class PlanillaPanel extends Panel {
             result.addAll(planillaModel.getObject().getJugadoresV());
             return result;
         }
+    }
+
+    private class DiaAdapterModel extends AbstractLocalDateTimeFormatModel {
+
+        public DiaAdapterModel(IModel<LocalDateTime> delegate) {
+            super(delegate);
+        }
+
+        @Override
+        protected DateTimeFormatter getFormatter() {
+            return DateTimeFormat.forPattern("dd");
+        }
+
+    }
+
+    private class MesAdapterModel extends AbstractLocalDateTimeFormatModel {
+
+        public MesAdapterModel(IModel<LocalDateTime> delegate) {
+            super(delegate);
+        }
+
+        @Override
+        protected DateTimeFormatter getFormatter() {
+            return DateTimeFormat.forPattern("MM");
+        }
+
+    }
+
+    private class AnoAdapterModel extends AbstractLocalDateTimeFormatModel {
+
+        public AnoAdapterModel(IModel<LocalDateTime> delegate) {
+            super(delegate);
+        }
+
+        @Override
+        protected DateTimeFormatter getFormatter() {
+            return DateTimeFormat.forPattern("YYYY");
+        }
+
     }
 }
