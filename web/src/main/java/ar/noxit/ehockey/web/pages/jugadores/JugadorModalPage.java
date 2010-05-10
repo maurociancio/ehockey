0068af047bf8d35373aa386dd9160f85493ac03b
Mauro Ciancio <maurociancio@gmail.com>
2010-05-10 18:22:24 -0300

    base para crear jugador on the fly
    see #161
    Time: 0h30m

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/jugadores/JugadorModalPage.java
@@ -0,0 +1,48 @@
+package ar.noxit.ehockey.web.pages.jugadores;
+
+import ar.noxit.ehockey.exception.JugadorExistenteException;
+import ar.noxit.ehockey.service.IJugadorService;
+import ar.noxit.ehockey.web.pages.base.AbstractColorBasePage;
+import ar.noxit.exceptions.NoxitException;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+public class JugadorModalPage extends AbstractColorBasePage {
+
+    @SpringBean
+    private IJugadorService jugadorService;
+
+    public JugadorModalPage(final ModalWindow modal) {
+        IModel<JugadorPlano> jugador = new Model<JugadorPlano>(new JugadorPlano());
+
+        add(new JugadorForm("formulario", jugador) {
+
+            @Override
+            protected void onSubmit(IModel<JugadorPlano> jugador) {
+                try {
+                    jugadorService.add(jugador.getObject());
+                } catch (JugadorExistenteException ex) {
+                    info("El jugador de "
+                            + jugador.getObject().getTipoDocumento()
+                            + " "
+                            + jugador.getObject().getNumeroDocumento()
+                                    .toString() + " ya existe en el sistema.");
+                } catch (NoxitException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        add(new AjaxLink<Void>("cerrar") {
+
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                modal.close(target);
+            }
+        });
+    }
+}
