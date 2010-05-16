a6c871e35c156b386769ef958ce2d62cbb50523d
Tito <tito@tito-laptop.(none)>
2010-05-16 20:40:16 -0300

    Bug fix, no compilaba planilla final.

--- web/src/main/java/ar/noxit/ehockey/model/EstadoPlanillaCargada.java
+++ web/src/main/java/ar/noxit/ehockey/model/EstadoPlanillaCargada.java
@@ -2,7 +2,6 @@ package ar.noxit.ehockey.model;
 
 import org.apache.commons.lang.Validate;
 
-import ar.noxit.ehockey.exception.PlanillaIncompletaException;
 import ar.noxit.ehockey.exception.ReglaNegocioException;
 import ar.noxit.ehockey.exception.TransicionEstadoInvalidaException;
 
@@ -12,10 +11,9 @@ public class EstadoPlanillaCargada extends EstadoPlanilla {
     public EstadoPlanilla publicar(PlanillaPublicable publicable) throws ReglaNegocioException {
         Validate.notNull(publicable);
 
-        if (publicable.checkPublicable()) {
-            return new EstadoPlanillaPublicada();
-        }
-        throw new PlanillaIncompletaException();
+        publicable.checkPublicable();
+        
+        return new EstadoPlanillaPublicada();
     }
 
     @Override
