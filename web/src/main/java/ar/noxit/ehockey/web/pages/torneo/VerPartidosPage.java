fbee160522e760eef51bf66b05dac2d3be590520
Mauro Ciancio <maurociancio@gmail.com>
2010-05-17 12:46:58 -0300

    mostrar fechas, rueda y partido en ver fixture

--- web/src/main/java/ar/noxit/ehockey/web/pages/torneo/VerPartidosPage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/torneo/VerPartidosPage.java
@@ -42,6 +42,9 @@ public class VerPartidosPage extends BaseTorneoPage {
         Validate.notNull(torneo, "torneo no puede ser null");
 
         List<IColumn<Partido>> columns = new ArrayList<IColumn<Partido>>();
+        columns.add(new PropertyColumn<Partido>(Model.of("Rueda"), "rueda"));
+        columns.add(new PropertyColumn<Partido>(Model.of("Fecha"), "fechaDelTorneo"));
+        columns.add(new PropertyColumn<Partido>(Model.of("Partido"), "partido"));
         columns.add(new PropertyColumn<Partido>(Model.of("Local"), "local.nombre"));
         columns.add(new PropertyColumn<Partido>(Model.of("Visitante"), "visitante.nombre"));
         columns.add(new AbstractLabelColumn<Partido>(Model.of("Inicio")) {
