0e0c96229905c882e9e63060e946d5d1640f6ffb
Mauro Ciancio <maurociancio@gmail.com>
2010-05-17 18:10:15 -0300

    mostrar estado de la planilla final
    see #65
    Time: 0h20m

--- web/src/main/java/ar/noxit/ehockey/web/pages/planilla/PlanillaPage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/planilla/PlanillaPage.java
@@ -6,9 +6,11 @@ import ar.noxit.ehockey.web.pages.base.AbstractHeaderPage;
 import ar.noxit.exceptions.NoxitException;
 import ar.noxit.exceptions.NoxitRuntimeException;
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
@@ -22,6 +24,15 @@ public class PlanillaPage extends AbstractHeaderPage {
 
         add(new BookmarkablePageLink<Void>("html_planilla", PlanillaPrinterFriendly.class,
                 new PageParameters(String.format("partido=%s,final=1", partidoId))));
+
+        add(new Label("estado", new AbstractReadOnlyModel<String>() {
+
+            @Override
+            public String getObject() {
+                Partido object = partido.getObject();
+                return object.getEstadoPlanilla();
+            }
+        }));
 
         add(new PlanillaPanel("panelPlanilla", new PlanillaModel(partido)));
 
