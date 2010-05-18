a409e6214ed2b8e10886d59eb00ca968d26e63bc
Tito <tito@tito-laptop.(none)>
2010-05-18 01:00:08 -0300

    Creando el panel base de usuarios
    see #15
    Time: 0h45m

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/usuarios/AltaUsuarioBasePanel.java
@@ -0,0 +1,23 @@
+package ar.noxit.ehockey.web.pages.usuarios;
+
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+
+import ar.noxit.ehockey.model.Usuario;
+
+public class AltaUsuarioBasePanel extends Panel {
+
+    private String password;
+
+    public AltaUsuarioBasePanel(String id, IModel<Usuario> usuario) {
+        super(id, usuario);
+
+        add(new RequiredTextField<String>("usuario", new PropertyModel<String>(usuario, "user")));
+        add(new PasswordTextField("contrasena", new PropertyModel<String>(usuario, "password")).setRequired(true));
+        add(new RequiredTextField<String>("nombre", new PropertyModel<String>(usuario, "nombre")));
+        add(new RequiredTextField<String>("apellido", new PropertyModel<String>(usuario, "apellido")));
+    }
+}
