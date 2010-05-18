fb738f7cbeba5f4a114e3fa4ff13b7359389090a
Tito <tito@tito-laptop.(none)>
2010-05-18 03:27:04 -0300

    Página de selección de tipo de usuario
    Esta página usa el provider y lleva a la página siguiente.
    Faltan crear los html de esta página y de las de alta.
    
    see #15
    Time: 0h15m

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/usuarios/TipoUsuarioPage.java
@@ -0,0 +1,39 @@
+package ar.noxit.ehockey.web.pages.usuarios;
+
+import java.util.List;
+
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import ar.noxit.ehockey.web.pages.base.AbstractHeaderPage;
+import ar.noxit.exceptions.NoxitException;
+import ar.noxit.web.wicket.model.LDM;
+
+public class TipoUsuarioPage extends AbstractHeaderPage{
+
+    @SpringBean
+    private IAltaUsuarioPagesProvider provider;
+    private String selected;
+
+    public TipoUsuarioPage() {
+        add(new DropDownChoice<String>("tipos", new PropertyModel<String>(this, "selected"), new PaginasUsuariosListModel()));
+
+        add(new Form<Void>("form"){
+
+            @Override
+            protected void onSubmit() {
+                setResponsePage(provider.getPagina(selected));
+            }
+        });
+    }
+
+    public class PaginasUsuariosListModel extends LDM<List<String>> {
+
+        @Override
+        protected List<String> doLoad() throws NoxitException {
+            return provider.getListaPaginas();
+        }
+    }
+}
