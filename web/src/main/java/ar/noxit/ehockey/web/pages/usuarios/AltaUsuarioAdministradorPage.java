ed41d8c0d677210a595229c4001c7a56528161c3
Tito <tito@tito-laptop.(none)>
2010-05-18 23:11:59 -0300

    Paginas de alta de usuarios
    Pagina de usuario administrador y de usuario representante

--- web/src/main/java/ar/noxit/ehockey/web/pages/usuarios/AltaUsuarioAdministradorPage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/usuarios/AltaUsuarioAdministradorPage.java
@@ -1,5 +1,25 @@
 package ar.noxit.ehockey.web.pages.usuarios;
 
-public class AltaUsuarioAdministradorPage extends AltaUsuarioPage {
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.PropertyModel;
 
+import ar.noxit.ehockey.model.Administrador;
+import ar.noxit.ehockey.model.Usuario;
+import ar.noxit.ehockey.web.pages.base.MensajePage;
+
+public class AltaUsuarioAdministradorPage extends AltaUsuarioPage {
+    private Administrador usuario;
+
+    public AltaUsuarioAdministradorPage() {
+        super();
+        add(new Form<Void>("usuarioPanel") {
+
+            @Override
+            protected void onSubmit() {
+                setResponsePage(new MensajePage("Alta de usuario", "Se ha completado el alta de usuario"));
+            }
+        });
+
+        add(new AltaUsuarioBasePanel("usuarioBasePanel", new PropertyModel<Usuario>(this, "usuario")));
+    }
 }
--- web/src/main/java/ar/noxit/ehockey/web/pages/usuarios/AltaUsuarioRepresentantePage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/usuarios/AltaUsuarioRepresentantePage.java
@@ -1,5 +1,26 @@
 package ar.noxit.ehockey.web.pages.usuarios;
 
-public class AltaUsuarioRepresentantePage extends AltaUsuarioPage {
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.PropertyModel;
 
+import ar.noxit.ehockey.model.Representante;
+import ar.noxit.ehockey.model.Usuario;
+import ar.noxit.ehockey.web.pages.base.MensajePage;
+
+public class AltaUsuarioRepresentantePage extends AltaUsuarioPage {
+    private Representante usuario;
+
+    public AltaUsuarioRepresentantePage() {
+        super();
+        add(new Form<Void>("usuarioPanel") {
+
+            @Override
+            protected void onSubmit() {
+                setResponsePage(new MensajePage("Alta de usuario", "Se ha completado el alta de usuario"));
+            }
+        });
+
+        add(new AltaUsuarioBasePanel("usuarioBasePanel", new PropertyModel<Usuario>(this, "usuario")));
+        add(new AltaUsuarioRepresentantePanel("usuarioRepresentantePanel", new PropertyModel<Representante>(this, "usuario")));
+    }
 }
