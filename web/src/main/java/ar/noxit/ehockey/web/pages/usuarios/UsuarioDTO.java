7dda93f5c89e1a7bdb0e09580367883074e16d68
Tito <tito@tito-laptop.(none)>
2010-05-19 22:27:52 -0300

    Creo el DTO de usuario
    see #15
    Time: 0h30m

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/usuarios/UsuarioDTO.java
@@ -0,0 +1,76 @@
+package ar.noxit.ehockey.web.pages.usuarios;
+
+import org.apache.wicket.model.IModel;
+
+import ar.noxit.ehockey.model.Club;
+
+public class UsuarioDTO {
+
+    private String user;
+    private String password;
+    private String nombre;
+    private String apellido;
+    private String cargo;
+    private IModel<Club> club;
+    private TipoUsuario tipo;
+
+    public UsuarioDTO(TipoUsuario tipo) {
+        this.tipo = tipo;
+    }
+
+    public TipoUsuario getTipo() {
+        return this.tipo;
+    }
+
+    public String getUser() {
+        return user;
+    }
+    
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Club getClub() {
+        return club.getObject();
+    }
+
+    public void setClubModel(IModel<Club> club) {
+        this.club = club;
+    }
+
+    public void setClub(Club club) {
+        this.club.setObject(club);
+    }
+}
