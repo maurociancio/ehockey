97859f9f1620c6082f2643fd8d3adcef8a4f7ce1
Mauro Ciancio <maurociancio@gmail.com>
2010-05-22 19:46:29 -0300

    borrar clase que ya estaba implementada
    see #15
    see #16
    see #17
    see #18
    Time: 0h10m

--- web/src/main/java/ar/noxit/ehockey/web/pages/usuarios/UsuarioRepresentantePanel.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/usuarios/UsuarioRepresentantePanel.java
@@ -1,5 +1,10 @@
 package ar.noxit.ehockey.web.pages.usuarios;
 
+import ar.noxit.ehockey.model.Club;
+import ar.noxit.ehockey.service.IClubService;
+import ar.noxit.ehockey.web.pages.models.ClubListModel;
+import ar.noxit.ehockey.web.pages.models.IdClubModel;
+import ar.noxit.ehockey.web.pages.renderers.ClubRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.panel.Panel;
@@ -8,41 +13,17 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
-import ar.noxit.ehockey.model.Club;
-import ar.noxit.ehockey.service.IClubService;
-import ar.noxit.ehockey.web.pages.models.ClubListModel;
-import ar.noxit.ehockey.web.pages.renderers.ClubRenderer;
-import ar.noxit.exceptions.NoxitException;
-import ar.noxit.web.wicket.model.IdLDM;
-
 public class UsuarioRepresentantePanel extends Panel {
 
     @SpringBean
-    IClubService clubService;
+    private IClubService clubService;
 
     public UsuarioRepresentantePanel(String id, IModel<UsuarioDTO> usuarioRepres) {
         super(id);
 
-        ClubModel club = new ClubModel(new Model<Integer>());
-        add(new DropDownChoice<Club>("clubes", club, new ClubListModel(clubService), new ClubRenderer()).setRequired(true));
+        IModel<Club> club = new IdClubModel(new Model<Integer>(), clubService);
+        add(new DropDownChoice<Club>("clubes", club, new ClubListModel(clubService), new ClubRenderer())
+                .setRequired(true));
         add(new RequiredTextField<String>("cargo", new PropertyModel<String>(usuarioRepres, "cargo")));
-    }
-
-    public class ClubModel extends IdLDM<Club, Integer> {
-
-        public ClubModel(IModel<Integer> idModel) {
-            super(idModel);
-        }
-
-        @Override
-        protected Club doLoad(Integer id) throws NoxitException {
-            return clubService.get(id);
-        }
-
-        @Override
-        protected Integer getObjectId(Club club) {
-            return club.getId();
-        }
-
     }
 }
