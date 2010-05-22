f3d799b0ee78a89e21151acc7b6244ff1c79eeb9
Mauro Ciancio <maurociancio@gmail.com>
2010-05-22 19:30:03 -0300

    clase base para usuarios page
    see #15
    see #18
    see #16
    Time: 0h15m

--- web/src/main/java/ar/noxit/ehockey/web/pages/usuarios/EditarUsuarioPage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/usuarios/EditarUsuarioPage.java
@@ -4,9 +4,7 @@ import org.apache.commons.lang.Validate;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.IModel;
 
-import ar.noxit.ehockey.web.pages.base.AbstractContentPage;
-
-public class EditarUsuarioPage extends AbstractContentPage{
+public class EditarUsuarioPage extends AbstractUsuariosPage {
 
     private IModel<UsuarioDTO> usuario;
 
@@ -16,14 +14,16 @@ public class EditarUsuarioPage extends AbstractContentPage{
 
         // creo los dos paneles. Uno por cada tipo de alta de usuario
         // los paneles solo se muestan si el usuario es del tipo que corresponde
-        FormularioAdministradorPanel adminPanel = new FormularioAdministradorPanel("administradorPanel", usuario, new EditarUsuarioForm(usuario));
-        FormularioRepresentantePanel represPanel = new FormularioRepresentantePanel("representantePanel", usuario, new EditarUsuarioForm(usuario));
+        FormularioAdministradorPanel adminPanel = new FormularioAdministradorPanel("administradorPanel", usuario,
+                new EditarUsuarioForm(usuario));
+        FormularioRepresentantePanel represPanel = new FormularioRepresentantePanel("representantePanel", usuario,
+                new EditarUsuarioForm(usuario));
         this.setOutputMarkupId(true);
 
         // panel para devolver errores de validación
         add(new FeedbackPanel("feedback"));
 
-        //hace que no se pueda cambiar el nombre de usuario en el panel
+        // hace que no se pueda cambiar el nombre de usuario en el panel
         adminPanel.setUsuarioEditable(false);
         represPanel.setUsuarioEditable(false);
 
