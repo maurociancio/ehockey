47ca83116b1c604991e0ae25a61dadd852eae535
Tito <njytito@yahoo.com.ar>
2010-05-23 21:13:22 -0300

    Alta del primer administrador
    fix #179
    Time: 1h0m

--- web/src/main/java/ar/noxit/ehockey/web/pages/authentication/LoginPage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/authentication/LoginPage.java
@@ -1,13 +1,70 @@
 package ar.noxit.ehockey.web.pages.authentication;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import ar.noxit.ehockey.model.Administrador;
+import ar.noxit.ehockey.model.Usuario;
+import ar.noxit.ehockey.service.IUsuarioService;
 import ar.noxit.ehockey.web.pages.base.AbstractColorBasePage;
 import ar.noxit.ehockey.web.pages.footer.FooterPanel;
+import ar.noxit.ehockey.web.pages.usuarios.AltaUsuarioForm;
+import ar.noxit.ehockey.web.pages.usuarios.FormularioAdministradorPanel;
+import ar.noxit.ehockey.web.pages.usuarios.TipoUsuario;
+import ar.noxit.ehockey.web.pages.usuarios.UsuarioDTO;
+import ar.noxit.exceptions.NoxitException;
 
 public class LoginPage extends AbstractColorBasePage {
+    @SpringBean
+    IUsuarioService usuarioService;
 
     public LoginPage() {
+        final IModel<UsuarioDTO> usuario = new Model<UsuarioDTO>();
+        //el panel de alta de usuario solo se muestra si no hay administrador
+        crearAdministradorSiNoExisteUno(usuario);
+
         add(new LoginHeaderPanel("header"));
-        add(new LoginPanel("signInPanel",true));
+        
+        add(new LoginPanel("signInPanel",true) {
+            @Override
+            public boolean isVisible() {
+                if (usuario.getObject() == null) return true;
+                return !usuario.getObject().getTipo().equals(Administrador.class);
+            }
+        });
+
+        add(new FormularioAdministradorPanel("usuario", usuario, new AltaUsuarioForm(usuario) {
+            @Override
+            protected void onSubmit() {
+                try {
+                    usuarioService.add(usuario.getObject());
+                    crearAdministradorSiNoExisteUno(usuario);
+                } catch (NoxitException e) {
+                }
+            }
+        }));
+
         add(new FooterPanel("footer"));
+    }
+
+    private void crearAdministradorSiNoExisteUno(IModel<UsuarioDTO> usuario) {
+        try {
+            List<Usuario> lista = usuarioService.getAll();
+            Iterator<Usuario> it = lista.iterator();
+            boolean encontrado = false;
+            while (it.hasNext() && !encontrado) {
+                if (it.next() instanceof Administrador) encontrado = true;
+            }
+            if (!encontrado)
+                usuario.setObject(new UsuarioDTO(new TipoUsuario(Administrador.class)));
+            else
+                usuario.setObject(null);
+        } catch (NoxitException e){
+            //no hago nada, muestra solo la pantalla de login si hay error
+        }
     }
 }
