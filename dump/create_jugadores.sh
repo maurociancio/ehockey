1e040a9c413a3091697b1904fd0011c9bb99f58d
Mauro Ciancio <maurociancio@gmail.com>
2010-05-25 18:17:58 -0300

    crear jugadores script

---
+++ dump/create_jugadores.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo $#
+if [ $# -ne 4 ];
+then
+	echo usage: create_jugadores.sh club_id base_ficha upper_ficha nombre_club
+	exit 1
+fi
+
+string=''
+
+for ficha in $( seq $2 $3 );
+do
+	echo $ficha
+	sql="insert into jugadores (ficha, apellido, nombre, tipodocumento, numerodocumento, fechanacimiento, activo, club_id, division_id, sector_id) values ($ficha, 'jugador $4 $ficha', 'apellido $4 $ficha', 'DNI', $1$ficha, '1900-01-01', TRUE, $1, 1, 1);"
+	string=$string$sql
+done
+
+echo $string
+psql --username ehockey -h localhost ehockey <<< $string
