1a99d47242707a3d590ff4ed8a8895d6d20ee242
lucas <lucasdle@gmail.com>
2010-05-29 12:33:36 -0300

    Agregado de link a la pagina principal de clubes see #152 see #153 see #155 Time: 0h10m

--- web/src/main/java/ar/noxit/ehockey/web/pages/clubes/ClubAltaPage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/clubes/ClubAltaPage.java
@@ -1,5 +1,11 @@
 package ar.noxit.ehockey.web.pages.clubes;
 
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+
 public class ClubAltaPage extends AbstractClubPage {
+    
+    public ClubAltaPage() {
+        add(new BookmarkablePageLink<AbstractClubPage>("volver", ClubPage.class));
+    }
 
 }
--- web/src/main/java/ar/noxit/ehockey/web/pages/clubes/ClubVerPage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/clubes/ClubVerPage.java
@@ -1,5 +1,11 @@
 package ar.noxit.ehockey.web.pages.clubes;
 
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+
 public class ClubVerPage extends AbstractClubPage {
+
+    public ClubVerPage() {
+        add(new BookmarkablePageLink<AbstractClubPage>("volver", ClubPage.class));
+    }
 
 }
