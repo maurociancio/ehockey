49f4056545f5ca0bca5b876ac8f30bcf37282a6b
lucas <lucasdle@gmail.com>
2010-05-29 17:15:20 -0300

    Getters para el BeanUtils en el Club y ClubPlano see #152 Time: 0h15m

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/clubes/ClubPlano.java
@@ -0,0 +1,106 @@
+package ar.noxit.ehockey.web.pages.clubes;
+
+import java.io.Serializable;
+
+public class ClubPlano implements Serializable {
+
+    private Integer id;
+    private String nombre;
+    private String nombreCompleto;
+    private String direccion;
+    private String ciudad;
+    private String codigoPostal;
+    private String provincia;
+    private String email;
+    private String telefono;
+    private String web;
+    private String observaciones;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+}
