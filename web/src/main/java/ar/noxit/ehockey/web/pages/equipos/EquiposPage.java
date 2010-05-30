38b90ed29c02122d94eb7f6348af54dfc473ad8b
Mauro Ciancio <maurociancio@gmail.com>
2010-05-30 19:47:53 -0300

    link a pagina de alta
    see #23
    Time: 0h10m

--- web/src/main/java/ar/noxit/ehockey/web/pages/equipos/EquiposPage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/equipos/EquiposPage.java
@@ -1,5 +1,10 @@
 package ar.noxit.ehockey.web.pages.equipos;
 
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+
 public class EquiposPage extends BaseEquipoPage {
 
+    public EquiposPage() {
+        add(new BookmarkablePageLink<Void>("alta", EquipoAltaPage.class));
+    }
 }
