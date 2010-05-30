728422a639f8ab965d9a8276cded3f9a7a02d23c
Mauro Ciancio <maurociancio@gmail.com>
2010-05-30 18:42:12 -0300

    implementar equipo plano
    see #23
    see #24
    see #25
    Time: 0h10m

---
+++ web/src/main/java/ar/noxit/ehockey/service/transfer/EquipoPlano.java
@@ -0,0 +1,52 @@
+package ar.noxit.ehockey.service.transfer;
+
+import java.io.Serializable;
+
+public class EquipoPlano implements Serializable {
+
+    private Integer id;
+    private String nombre;
+    private Integer divisionId;
+    private Integer clubId;
+    private Integer sectorId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getDivisionId() {
+        return divisionId;
+    }
+
+    public void setDivisionId(Integer divisionId) {
+        this.divisionId = divisionId;
+    }
+
+    public Integer getClubId() {
+        return clubId;
+    }
+
+    public void setClubId(Integer clubId) {
+        this.clubId = clubId;
+    }
+
+    public Integer getSectorId() {
+        return sectorId;
+    }
+
+    public void setSectorId(Integer sectorId) {
+        this.sectorId = sectorId;
+    }
+}
