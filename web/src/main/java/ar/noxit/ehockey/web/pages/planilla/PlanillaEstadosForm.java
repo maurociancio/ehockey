0d386654f73363634c754c689bdabadd6c7bfc70
Mauro Ciancio <maurociancio@gmail.com>
2010-06-04 01:10:24 -0300

    mostrar form para cambiar estados si la planilla es editable y es local
    Time: 0h15m
    see #203

--- web/src/main/java/ar/noxit/ehockey/web/pages/planilla/PlanillaEstadosForm.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/planilla/PlanillaEstadosForm.java
@@ -1,17 +1,24 @@
 package ar.noxit.ehockey.web.pages.planilla;
 
+import ar.noxit.ehockey.model.Club;
 import ar.noxit.ehockey.model.Partido;
 import ar.noxit.ehockey.model.PlanillaFinal;
+import ar.noxit.ehockey.model.Usuario;
+import ar.noxit.ehockey.web.pages.authentication.AuthSession;
+import ar.noxit.ehockey.web.pages.authentication.IRenderable;
+import org.apache.wicket.authorization.strategies.role.Roles;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.IModel;
 
-public class PlanillaEstadosForm extends Form<Void> {
+public class PlanillaEstadosForm extends Form<Void> implements IRenderable {
 
     private final IModel<PlanillaFinal> planillaModel;
+    private IModel<Partido> partido;
 
     public PlanillaEstadosForm(String id, IModel<PlanillaFinal> planillaModel, IModel<Partido> partido) {
         super(id);
         this.planillaModel = planillaModel;
+        this.partido = partido;
 
         add(new PublicarPlanillaButton("publicar", planillaModel, partido));
         add(new EditarPlanillaButton("editar", planillaModel, partido));
@@ -19,7 +26,14 @@ public class PlanillaEstadosForm extends Form<Void> {
     }
 
     @Override
-    public boolean isEnabled() {
-        return !planillaModel.getObject().isFinalizada();
+    public boolean isVisible() {
+        return planillaModel.getObject().isEditable();
+    }
+
+    @Override
+    public boolean couldBeRendered(Roles roles) {
+        Usuario userLogged = AuthSession.get().getUserLogged();
+        Club clubVisitante = partido.getObject().getLocal().getClub();
+        return userLogged.puedeVer(clubVisitante);
     }
 }
