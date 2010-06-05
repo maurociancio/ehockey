dfbc239e6aec147f909ee5450d0b15222f444744
lucas <lucasdle@gmail.com>
2010-06-05 20:28:15 -0300

    Modificacion en el dao para verificar si existe club por nombre see #152 see #153 Time: 0h8m

--- web/src/main/java/ar/noxit/ehockey/dao/IClubDao.java
+++ web/src/main/java/ar/noxit/ehockey/dao/IClubDao.java
@@ -1,7 +1,12 @@
 package ar.noxit.ehockey.dao;
 
-import ar.noxit.ehockey.model.Club;
+import java.util.List;
+
 import ar.noxit.dataaccessobject.IDao;
+import ar.noxit.ehockey.model.Club;
+import ar.noxit.ehockey.model.Jugador;
 
 public interface IClubDao extends IDao<Club, Integer> {
+
+    public List<Jugador> getClubPorNombre(String nombre, String nombreCompleto);
 }
