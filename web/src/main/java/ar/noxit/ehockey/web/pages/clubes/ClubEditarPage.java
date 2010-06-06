62cbe62f6d8e9bc2615a0039eb9545e2ad496dfa
lucas <lucasdle@gmail.com>
2010-06-05 21:04:22 -0300

    Aplicar la verificacion a la modificacion de clubes see #153 Time: 0h5m

--- web/src/main/java/ar/noxit/ehockey/web/pages/clubes/ClubEditarPage.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/clubes/ClubEditarPage.java
@@ -3,6 +3,7 @@ package ar.noxit.ehockey.web.pages.clubes;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import ar.noxit.ehockey.exception.ClubYaExistenteException;
 import ar.noxit.ehockey.service.IClubService;
 import ar.noxit.exceptions.NoxitException;
 
@@ -17,8 +18,11 @@ public class ClubEditarPage extends AbstractMainLinkedClubPage {
             @Override
             public void onSubmit(IModel<ClubPlano> clubPlano) {
                 try {
+                    clubService.verificarCambioNombre(clubPlano.getObject());
                     clubService.update(clubPlano.getObject());
                     setResponsePage(ClubVerPage.class);
+                } catch (ClubYaExistenteException e) {
+                    info("No se puede cambiar el nombre, ya existe otro club con ese nombre.");
                 } catch (NoxitException e) {
                     info("No se pudo editar el club.");
                 }
