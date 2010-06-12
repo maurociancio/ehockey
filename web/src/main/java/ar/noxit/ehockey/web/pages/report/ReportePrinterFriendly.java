cab50c7de37368d0fa127e5279b526eeaa8cc9a4
lucas <lucasdle@gmail.com>
2010-06-12 19:00:54 -0300

    Inicio de pagina printer friendly de reporte
    see #214
    Time: 0h5m

---
+++ web/src/main/java/ar/noxit/ehockey/web/pages/report/ReportePrinterFriendly.java
@@ -0,0 +1,10 @@
+package ar.noxit.ehockey.web.pages.report;
+
+import ar.noxit.ehockey.web.pages.base.AbstractColorLessBasePage;
+
+public class ReportePrinterFriendly extends AbstractColorLessBasePage {
+    
+    public ReportePrinterFriendly() {
+    }
+
+}
