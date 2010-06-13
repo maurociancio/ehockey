af771b9f0e0d62b24e73578c1c3338346a4c926d
lucas <lucasdle@gmail.com>
2010-06-12 23:16:28 -0300

    Fix en el disenio
    see #214
    Time: 0h4m

--- web/src/main/java/ar/noxit/ehockey/web/pages/report/ReportePrinterFriendly.java
+++ web/src/main/java/ar/noxit/ehockey/web/pages/report/ReportePrinterFriendly.java
@@ -1,10 +1,8 @@
 package ar.noxit.ehockey.web.pages.report;
 
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import ar.noxit.ehockey.model.Jugador;
-import ar.noxit.ehockey.service.IJugadorService;
 import ar.noxit.ehockey.web.pages.base.AbstractColorLessBasePage;
 
 public class ReportePrinterFriendly extends AbstractColorLessBasePage {
